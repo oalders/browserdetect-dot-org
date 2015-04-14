93f5705d6ad3bf72dc864fa9b63d67c1380cecdf
Olaf Alders <olaf@wundersolutions.com>
2015-04-14 18:31:43 -0400

    Move scripts to bin directory.

---
+++ bin/startup.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use BDO::App;
+
+BDO::App::to_app();
---
+++ lib/BDO/App.pm
@@ -0,0 +1,30 @@
+package BDO::App;
+
+use strict;
+use warnings;
+
+use Plack::Builder;
+use Plack::Middleware::Static;
+
+sub to_app {
+    my $app = builder {
+        enable 'Plack::Middleware::Static',
+            path => qr{^/(css|fonts|js)/},
+            root => './static/';
+        mount '/' => builder {
+            require 'bin/app.pl';
+        };
+    };
+
+    return $app;
+}
+
+1;
+
+=pod
+
+=head1 DESCRIPTION
+
+This file exists solely for wrapping the app with middleware.
+
+=cut
